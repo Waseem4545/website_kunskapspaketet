06d944197442dd553160269200202647584d8fc2
Ghlawnji Basheer <Basheer.Ghlawnji@vaxjo.se>
2020-05-15 10:24:09 +0200

    add react Router

--- project/src/App.js
+++ project/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import './styles/css/main.css'
-/*import Main from './views/main' */
-import Home from './views/home'
+import React from "react";
+
+/* thired part packages */
+
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+
+/* our components */
+import "./styles/css/main.css";
+import Main from './views/main';
+import Home from "./views/home";
 
 function App() {
   return (
-    <div className="app">
-      {/* <Main /> */}
-        <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/hem" component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- project/src/components/navbar_mb.jsx
+++ project/src/components/navbar_mb.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
+import {Link} from 'react-router-dom';
+
 const Mobile_navbar = (props) => {
   return (
     <div className="mobile-navbar">
         <ul>
-            <li><i class="fa fa-arrow-left fa-2x" aria-hidden="true"></i></li>
-            <li><i class="fa fa-home fa-2x" aria-hidden="true"></i></li>
-            <li><i class="fa fa-arrow-right fa-2x" aria-hidden="true"></i></li>
+            <li><i class="fa fa-sign-out fa-lg" aria-hidden="true"></i></li>
+            <Link to="/hem">
+              <li><i class="fa fa-home fa-lg" aria-hidden="true"></i></li>
+            </Link>
+            
+            <li><i class="fa fa-arrow-left fa-lg" aria-hidden="true"></i></li>
         </ul>
     </div>
   );
