3b145c58e530b1a27badb736a9ec81e9f17fa745
Jens Andersson <jensandersson112@gmail.com>
2020-05-27 08:33:29 +0200

    Updates

---
+++ precache-manifest.dab88c21cde279e1113a9542ba3ae063.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b34e75b800ec7208aa02e627832b634",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3effbd7095f60c4eb7e",
+    "url": "/static/css/main.e23d9a67.chunk.css"
+  },
+  {
+    "revision": "7e7d58e8a5dda071e286",
+    "url": "/static/js/2.eefdffd5.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.eefdffd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3effbd7095f60c4eb7e",
+    "url": "/static/js/main.b83e9d58.chunk.js"
+  },
+  {
+    "revision": "ce55d074dd7ea4545b96",
+    "url": "/static/js/runtime-main.f98aef0b.js"
+  },
+  {
+    "revision": "89a5d4f25e1bf0a86a24457ce91e469d",
+    "url": "/static/media/fullscreen_mode.89a5d4f2.png"
+  }
+]);
