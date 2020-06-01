d79effda780381cc60605f0845fc46f23b268836
Jens Andersson <jensandersson112@gmail.com>
2020-06-01 08:34:34 +0200

    Feature/settings (#22)
    * fixed topbar css
    
    * added settings firebase connections

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const topbar = ({ name, backLink, color }) => {
+const topbar = ({ name, backLink, color, info }) => {
   return (
     <div className="topbar mb-2" style={{ background: color }}>
-      <Link to={backLink}>
-        <i className="fa fa-arrow-left ml-4 fa-lg"></i>
-      </Link>
-      <h3 className="text-center text-white m-0">{name}</h3>
-      <i className="fa fa-info-circle fa-lg mr-4"></i>
+      <h3 className="text-center text-white m-0 w-100 position-relative">
+        {backLink && (
+          <Link to={backLink} className="position-absolute back-button">
+            <i className="fa fa-arrow-left ml-4 fa-xs"></i>
+          </Link>
+        )}
+        {name}
+        {info && (
+          <button className="position-absolute info-button">
+            <i className="fa fa-info-circle mr-4 fa-xs"></i>
+          </button>
+        )}
+      </h3>
     </div>
   );
 };
